649d8273c2113d244a545b96dd1b68d419db61d1
xavikortes <javiercortesveliz@gmail.com>
2019-07-05 12:46:46 +0200

    Quitando acciones aqnuser sin sentido ni redireccion

--- model_fllogin__aqn_user.py
+++ model_fllogin__aqn_user.py
@@ -9,32 +9,6 @@ from models.fllogin import models as modelos
 class interna_aqn_user(modelos.mtd_aqn_user, helpers.MixinConAcciones):
     pass
 
-    @helpers.decoradores.accion(aqparam=["oParam"])
-    def getUsuariosProyecto(self, oParam):
-        return form.iface.getUsuariosProyecto(oParam)
-
-    @helpers.decoradores.accion(aqparam=["oParam"])
-    def getParticipantesProyecto(self, oParam):
-        return form.iface.getParticipantesProyecto(oParam)
-
-    @helpers.decoradores.accion(aqparam=["oParam"])
-    def getParticProyectosUsu(self, oParam):
-        return form.iface.getParticProyectosUsu(oParam)
-
-    @helpers.decoradores.accion(aqparam=["oParam"])
-    def getParticCompaniaUsu(self, oParam):
-        return form.iface.getParticCompaniaUsu(oParam)
-
-    @helpers.decoradores.accion(aqparam=["oParam", "cursor"])
-    def desactivar_usuario(self, oParam, cursor):
-        return form.iface.desactivar_usuario(self, oParam, cursor)
-
-    def checkCambiaPassword(cursor):
-        return form.iface.checkCambiaPassword(cursor)
-
-    def checkDrawUser(cursor):
-        return form.iface.checkDrawUser(cursor)
-
     class Meta:
         proxy = True
 
